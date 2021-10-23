162e415790e20878febd06e4fba777430be3456b
Crepiks <sayazhan.onlassyn@mail.ru>
2021-10-23 18:26:14 +0600

    feat: initialize typeorm database connection

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -1,6 +1,17 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'root',
+      database: 'nomad',
+      synchronize: true,
+    }),
+  ],
 })
 export class AppModule {}
