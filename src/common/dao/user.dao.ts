3001eb1cfe5ece0a3d7ffec2c444a286b4f67a02
Crepiks <sayazhan.onlassyn@mail.ru>
2021-10-24 11:50:31 +0600

    feat: make not required columns of users table nullable

--- src/common/dao/user.dao.ts
+++ src/common/dao/user.dao.ts
@@ -12,24 +12,24 @@ export class UserDao {
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
-  @Column({ name: 'first_name', nullable: true })
-  firstName: string;
+  @Column({ name: 'first_name', type: 'varchar', length: 255, nullable: true })
+  firstName?: string;
 
-  @Column({ name: 'last_name', nullable: true })
-  lastName: string;
+  @Column({ name: 'last_name', type: 'varchar', length: 255, nullable: true })
+  lastName?: string;
 
-  @Column({ name: 'phone' })
+  @Column({ name: 'phone', type: 'varchar', length: 20, unique: true })
   phone: string;
 
-  @Column({ name: 'password' })
+  @Column({ name: 'password', type: 'varchar', length: 255 })
   password: string;
 
   @UpdateDateColumn({ name: 'updated_at' })
-  updatedAt: string;
+  updatedAt?: string;
 
   @CreateDateColumn({ name: 'created_at' })
   createdAt: string;
 
   @DeleteDateColumn({ name: 'deleted_at' })
-  deletedAt: string;
+  deletedAt?: string;
 }
--- src/common/entities/user.ts
+++ src/common/entities/user.ts
@@ -1,10 +1,10 @@
 export class User {
   id: string;
-  firstName: string;
-  lastName: string;
+  firstName?: string;
+  lastName?: string;
   phone: string;
   password: string;
-  updatedAt: string;
+  updatedAt?: string;
   createdAt: string;
-  deletedAt: string;
+  deletedAt?: string;
 }
